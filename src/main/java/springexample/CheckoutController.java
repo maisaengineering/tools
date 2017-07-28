bc9fdc90ebe86f6a487a420607b35eb5b47fd431
Chandan Benjaram <cbenjaram@apple.com>
2017-07-27 17:44:50 -0700

    synch

--- src/main/java/springexample/CheckoutController.java
+++ src/main/java/springexample/CheckoutController.java
@@ -3,6 +3,10 @@ package springexample;
 import java.math.BigDecimal;
 import java.util.Arrays;
 
+import javax.servlet.http.HttpSession;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -39,7 +43,7 @@ public class CheckoutController {
 	public String registerMerchant() {
 		MerchantAccountRequest request = new MerchantAccountRequest()
 				.individual().firstName("Scott").lastName("Powers")
-				.email("scott.powers21@gmail.com").phone("7068881079")
+				.email("chandan.benjaram@gmail.com").phone("7068881079")
 				.dateOfBirth("1966-03-21").address()
 				.streetAddress("6730 Crofton Dr.").locality("Alpharetta")
 				.region("GA").postalCode("30005").done().done().business()
@@ -49,9 +53,10 @@ public class CheckoutController {
 				.locality("Alpharetta").region("GA").postalCode("30005").done()
 				.done().funding().descriptor("from www.tingr.org")
 				.destination(FundingDestination.BANK)
-				.email("scott.powers21@gmail.com").accountNumber("3650511177")
-				.routingNumber("051400549").done().tosAccepted(true)
-				.id("kidslink").masterMerchantAccountId("TINGR_marketplace");
+				.email("chandan.benjaram@gmail.com")
+				.accountNumber("3650511177").routingNumber("051400549").done()
+				.tosAccepted(true).id("cb1")
+				.masterMerchantAccountId("maisasolutionsinc");
 
 		Result<MerchantAccount> result = gateway.merchantAccount().create(
 				request);
@@ -60,9 +65,54 @@ public class CheckoutController {
 
 	}
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String root(Model model) {
-		return "redirect:checkouts";
+	@RequestMapping(value = "/token", method = RequestMethod.GET)
+	public String utils(HttpSession session) {
+		String clientToken = gateway.clientToken().generate();
+		session.setAttribute("data", clientToken);
+
+		return "utils/show";
+	}
+
+	@RequestMapping(value = "/sale", method = RequestMethod.GET)
+	public String sale(HttpSession session, @RequestParam("nonce") String nonce) {
+		System.out.println("NONCE..." + nonce);
+		if (nonce == null || nonce.trim().length() <= 0) {
+			new ResponseEntity<String>(String.valueOf("missing nonce..."),
+					HttpStatus.OK);
+		}
+
+		TransactionRequest request = new TransactionRequest()
+				.merchantAccountId("cb1").amount(new BigDecimal("10.00"))
+				.serviceFeeAmount(new BigDecimal("1.00"))
+				.paymentMethodNonce(nonce)
+				.customField("facility", "my facility..." + Math.random())
+				.customField("season", "testing season..." + Math.random())
+				.descriptor().name("REG-FEE-MOSTERSINC").done().options()
+				.submitForSettlement(true).holdInEscrow(true).done();
+
+		Result<Transaction> result = gateway.transaction().sale(request);
+		Transaction transaction = result.getTransaction();
+		String responseStr = result.getMessage()
+				+ (result.isSuccess() ? "...success" : "...failed")
+				+ "...kind -> " + request.creditCard().getKind();
+		if (!result.isSuccess() && transaction != null) {
+			StringBuffer str = new StringBuffer();
+			str.append("\r\n getTaxAmount..." + transaction.getTaxAmount());
+			str.append("\r\n getAuthorizedTransactionId..."
+					+ transaction.getAuthorizedTransactionId());
+			str.append("\r\n getStatus..." + transaction.getStatus());
+			str.append("\r\n getAvsErrorResponseCode..."
+					+ transaction.getAvsErrorResponseCode());
+			str.append("\r\n getAvsPostalCodeResponseCode..."
+					+ transaction.getAvsPostalCodeResponseCode());
+
+			str.append("\r\n getCvvResponseCode..."
+					+ transaction.getCvvResponseCode());
+			responseStr = str.toString();
+		}
+
+		session.setAttribute("data", responseStr);
+		return "utils/show";
 	}
 
 	@RequestMapping(value = "/checkouts", method = RequestMethod.GET)
